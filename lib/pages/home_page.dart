aa8b4666f03958e6590bc43a982867a9bf81285b
Bishal <prakashbishal1973@gmail.com>
2024-04-21 12:05:52 +0530

    Added the padding in the listview

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -66,43 +66,47 @@ class _HomePageState extends State<HomePage> {
 
   Widget _tasksList() {
     List tasks = _box!.values.toList();
-    return ListView.builder(
-      itemCount: tasks.length,
-      itemBuilder: (BuildContext _context, int _index) {
-        var task = Task.fromMap(tasks[_index]);
-        return ListTile(
-          title: Text(
-            task.content,
-            style: TextStyle(
-              decoration: task.done ? TextDecoration.lineThrough : null,
-              fontSize: 25,
-              color: Color.fromRGBO(19, 28, 133, 1),
-              fontWeight: FontWeight.bold,
+    return Padding(
+      padding:
+          const EdgeInsets.only(bottom: 80.0), // Adjust this value as needed
+      child: ListView.builder(
+        itemCount: tasks.length,
+        itemBuilder: (BuildContext _context, int _index) {
+          var task = Task.fromMap(tasks[_index]);
+          return ListTile(
+            title: Text(
+              task.content,
+              style: TextStyle(
+                decoration: task.done ? TextDecoration.lineThrough : null,
+                fontSize: 25,
+                color: Color.fromRGBO(19, 28, 133, 1),
+                fontWeight: FontWeight.bold,
+              ),
             ),
-          ),
-          subtitle: Text(
-            task.timestamp.toString(),
-          ),
-          trailing: Icon(
-            task.done
-                ? Icons.check_box_outlined
-                : Icons.check_box_outline_blank_outlined,
-            color: Colors.red,
-          ),
-          onTap: () {
-            task.done = !task.done;
-            _box!.putAt(
-              _index,
-              task.toMap(),
-            );
-            setState(() {});
-          },
-          onLongPress: () {
-            _box!.deleteAt(_index);
-            setState(() {});
-          },
-        );
-      },
+            subtitle: Text(
+              task.timestamp.toString(),
+            ),
+            trailing: Icon(
+              task.done
+                  ? Icons.check_box_outlined
+                  : Icons.check_box_outline_blank_outlined,
+              color: Colors.red,
+            ),
+            onTap: () {
+              task.done = !task.done;
+              _box!.putAt(
+                _index,
+                task.toMap(),
+              );
+              setState(() {});
+            },
+            onLongPress: () {
+              _box!.deleteAt(_index);
+              setState(() {});
+            },
+          );
+        },
+      ),
     );
   }
 
